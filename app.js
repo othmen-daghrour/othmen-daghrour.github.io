c739b28cd75a7754fd0e475a62eda7e2f0b46c1f
othmen-daghrour <othmen.daghrour@hotmail.com>
2023-09-19 18:50:45 +0200

    Portfolio Version 2.0

---
+++ app.js
@@ -0,0 +1,38 @@
+// const ArrayOfTools=['React','Node.js','TypeScript','JavaScript','TypeORM','MongoDB','PostgreSQL','Redis','GraphQL','SASS'];
+// const Projects_List = document.getElementById('Projects_List')
+
+// let ArrayOfToolsClone=ArrayOfTools;
+
+// let skills = document.getElementById('skills');
+// function build (){
+//     for(let i =0; i<ArrayOfToolsClone.length;i++)
+//     {
+//     let div = document.createElement('div');
+//     div.innerText=ArrayOfTools[i];
+//     Projects_List.append(div);
+//     }
+// }
+
+// build();
+
+// skills.addEventListener('keyup',(e)=>{
+//     console.log(e.target.value);
+//     ArrayOfToolsClone=ArrayOfTools.filter((skill)=>{
+//         return skill==e.target.value;
+//     })
+//     if(ArrayOfToolsClone.length > 0){
+//         Projects_List.innerHTML='<span><span/>'
+//         for(let i =0; i<ArrayOfToolsClone.length;i++)
+//             {
+//             let div = document.createElement('div');
+//             div.innerText=ArrayOfToolsClone[i];
+//             Projects_List.append(div);
+//             }
+//         }
+//         else
+//         {
+//             build();
+//         }
+   
+// });
+
